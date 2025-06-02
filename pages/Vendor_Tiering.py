3b62a54014e303d5b2be6022ca25fe4d08c5cb8a
Geo Perdana Saragih <161802894+geosimarmata@users.noreply.github.com>
2025-06-02 17:30:06 +0700

    Another Update Save File Named based on Shipper

--- pages/Vendor_Tiering.py
+++ pages/Vendor_Tiering.py
@@ -287,6 +287,6 @@ if st.session_state.tiered_df is not None:
     st.download_button(
         label="⬇️ Download Filtered CSV",
         data=filtered_df.to_csv(index=False),
-        file_name=f"{desired_sheets.replace(' ', '_').replace('!', '').lower()}_tiered_vendor.csv",
+        file_name=f"{st.session_state.sheet_name.replace(' ', '_').replace('!', '').lower()}_tiered_vendor.csv",
         mime="text/csv"
     )
